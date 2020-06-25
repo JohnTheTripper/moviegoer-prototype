e678b2f68b86fd28c952d6bfcabd0a7349e8cf01
JohnTheTripper <minutehand@gmail.com>
2020-06-25 17:54:49 -0400

    better way of extracting frames to avoid frame-drift of 23.976p video files

--- extract.py
+++ extract.py
@@ -2,7 +2,7 @@ import cv2
 import os
 
 # create frames from video file
-title = 'extremely_wicked'
+title = 'hobbs_shaw'
 scale_percent = 100     # optional image scaling
 
 cap = cv2.VideoCapture('input_videos/' + title + '.mkv')
@@ -13,17 +13,17 @@ if not cap.isOpened():
     print("Error opening video stream or file")
 
 frame_rate = cap.get(5)              # frame rate
-print('Video frame rate:', frame_rate)
-frame_rate_int = round(frame_rate)             # rounds up the frame_rate of 23.976 to 24 for cleaner frame extraction
-print('Extracting image every:', frame_rate_int, 'frames')
+print('Video frame rate:', frame_rate)  # most movie files will have a frame_rate of 23.976 frames per second
 
-count = 1   # starts at 1, because first image isn't saved until 24th frame (timestamp of 1 second)
+
+# because of the 23.976 fps frame_rate, we can't just capture every 24 frames
+count = 1   # starts at 1, because first image isn't saved until 25th frame (timestamp of 1.001 seconds)
 while cap.isOpened():
     ret, frame = cap.read()
 
     if not ret:
         break
-    if cap.get(1) % frame_rate_int == 0:     # frame every 1 second
+    if cap.get(0) / 1000 >= count:     # first frame after 1 second, 2 seconds, etc.
         filename = output_directory + '/' + title + '_frame%d.jpg' % count
 
         # optional image scaling
