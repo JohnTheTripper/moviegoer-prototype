280558cd696db3922bcb9d25c54d3b061b01d397
JohnTheTripper <minutehand@gmail.com>
2020-10-07 04:16:40 -0400

    moved face-related functions into dedicated functions file

---
+++ vision_features/faces_io.py
@@ -0,0 +1,92 @@
+from vision_features_io import *
+import face_recognition
+import math
+
+
+def primary_character_flag(locations):
+    if len(locations) == 1:
+        return 1
+    else:
+        local_locations = locations.copy()
+        first_face = local_locations.pop(0)
+        first_face_size = first_face[2] - first_face[0]
+        primary_char_flag = 1
+        primary_char_threshold = .75  # 75% of the first face
+        for face in local_locations:
+            if face[2] - face[0] > (first_face_size * primary_char_threshold):
+                return 0
+        else:
+            return 1
+
+
+def third_points_alignment(character, frame):
+    thirds_point_a, thirds_point_b, thirds_point_c, thirds_point_d = thirds_points(frame)
+
+    if character[3] <= thirds_point_a[0] <= character[1] and character[0] <= thirds_point_a[1] <= character[2]:
+        return 'left'
+    elif character[3] <= thirds_point_b[0] <= character[1] and character[0] <= thirds_point_b[1] <= character[2]:
+        return 'right'
+    else:
+        return None
+
+
+def get_face_size(locations, frame):
+    face_size = pow((locations[1] - locations[3]), 2)
+    image_size = frame.shape[0] * frame.shape[1]
+
+    return round(face_size / image_size, 4)
+
+
+def analyze_mouth_open(frame_folder, film, frame_choice):
+    """
+    returns a list of flags, for if a character has their mouth open, in each frame
+    returns a 0 if no face detected in frame
+    """
+    mouth_open_list = []
+
+    for x in frame_choice:
+        img_path = frame_folder + '/' + film + '_frame' + str(x) + '.jpg'
+        image = face_recognition.load_image_file(img_path)
+        face_locations = face_recognition.face_locations(image, number_of_times_to_upsample=1)
+        face_landmarks_list = face_recognition.face_landmarks(image, face_locations)
+
+        # print('Found ' + str(len(face_locations)) + ' face(s) in frame ' + str(x))
+        frame_encodings_list = face_recognition.face_encodings(image, face_locations)
+
+        if frame_encodings_list:
+            face_landmarks = face_landmarks_list[0]
+            mouth_open_list.append(mouth_open_check(face_landmarks))
+        else:
+            mouth_open_list.append(0)
+
+    return mouth_open_list
+
+
+def get_lip_height(lip):
+    for i in [2, 3, 4]:
+        sum = 0
+        distance = math.sqrt((lip[i][0] - lip[12-i][0])**2 + (lip[i][1] - lip[12-i][1])**2)
+        sum += distance
+    return sum / 3
+
+
+def get_mouth_height(top_lip, bottom_lip):
+    for i in [8, 9, 10]:
+        sum = 0
+        distance = math.sqrt((top_lip[i][0] - bottom_lip[18-i][0])**2 + (top_lip[i][1] - bottom_lip[18-i][1])**2)
+        sum += distance
+    return sum / 3
+
+
+def mouth_open_check(face_landmarks, open_ratio=.8):
+    top_lip = face_landmarks['top_lip']
+    bottom_lip = face_landmarks['bottom_lip']
+
+    top_lip_height = get_lip_height(top_lip)
+    bottom_lip_height = get_lip_height(bottom_lip)
+    mouth_height = get_mouth_height(top_lip, bottom_lip)
+
+    if mouth_height > min(top_lip_height, bottom_lip_height) * open_ratio:
+        return 1
+    else:
+        return 0
--- vision_features/vision_features_io.py
+++ vision_features/vision_features_io.py
@@ -6,6 +6,7 @@ import face_recognition
 
 # loading frames
 
+
 def load_frame(film, frame_number):
     frame_folder = os.path.join('../frame_per_second', film)
     img_path = frame_folder + '/' + film + '_frame' + str(frame_number) + '.jpg'
@@ -34,6 +35,16 @@ def aspect_ratio(frame):
     return round(width / height, 2)
 
 
+def blank_frame(frame):
+    brightest_pixel = max_brightness(frame)
+    if frame.mean() < 3:  # threshold of 3, to be safe
+        return 'black'
+    elif frame.mean() > brightest_pixel * .95:
+        return 'white'
+    else:
+        return None
+
+
 def center_point(frame):
     height = frame.shape[0]
     width = frame.shape[1]
@@ -54,10 +65,10 @@ def thirds_points(frame):
     one_third_width = round(width * (1 / 3))
     two_thirds_width = round(width * (2 / 3))
 
-    thirds_point_a = (one_third_width, one_third_height)
-    thirds_point_b = (two_thirds_width, one_third_height)
-    thirds_point_c = (one_third_width, two_thirds_height)
-    thirds_point_d = (two_thirds_width, two_thirds_height)
+    thirds_point_a = (one_third_width, one_third_height)            # upper-left
+    thirds_point_b = (two_thirds_width, one_third_height)           # upper-right
+    thirds_point_c = (one_third_width, two_thirds_height)           # bottom-left
+    thirds_point_d = (two_thirds_width, two_thirds_height)          # bottom-right
 
     return thirds_point_a, thirds_point_b, thirds_point_c, thirds_point_d
 
@@ -133,11 +144,40 @@ def true_width(frame):
 
 
 def true_aspect_ratio(frame):
-
+    if blank_frame(frame) == 'black' or blank_frame(frame) == 'white':
+        return aspect_ratio(frame)
     return round(true_width(frame) / true_height(frame), 2)
 
 
 # chroma and luma
+
+def mean_brightness(frame):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    return round(gray.mean(), 2)
+
+
+def calculate_contrast(frame):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    return round(gray.std(), 2)
+
+
+def max_brightness(frame):
+    brightest_pixel = 0
+    # brightest_coordinate = 0
+    x = 0
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    for a in gray:
+        y = 0
+        for b in a:
+            if b > brightest_pixel:
+                brightest_pixel = b
+                # brightest_coordinate = (x, y)
+            y += 1
+        x += 1
+    return brightest_pixel
+
 
 def brightest_pixel_intensity(frame):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -196,26 +236,30 @@ def bgr(unrowed):
 
 
 def dominant_color(frame):
+    if frame.mean() < 50:
+        return None
     mid_pixels = remove_highlights_shadows(frame)
+    mid_pixels_mean = mid_pixels.mean()
     b, g, r = bgr(mid_pixels)
+    b_mean = b.mean()
+    g_mean = g.mean()
+    r_mean = r.mean()
     primary_threshold = .5
     secondary_threshold = .1
-    if frame.mean() < 30:
-        return 0
-    elif b.mean() / (mid_pixels.mean() * 3) > primary_threshold:
+    if b_mean / (mid_pixels_mean * 3) > primary_threshold:
         return 'blue'
-    elif g.mean() / (mid_pixels.mean() * 3) > primary_threshold:
+    elif g_mean / (mid_pixels_mean * 3) > primary_threshold:
         return 'green'
-    elif r.mean() / (mid_pixels.mean() * 3) > primary_threshold:
+    elif r_mean / (mid_pixels_mean * 3) > primary_threshold:
         return 'red'
-    elif b.mean() / (mid_pixels.mean() * 3) < secondary_threshold:
+    elif b_mean / (mid_pixels_mean * 3) < secondary_threshold:
         return 'yellow'
-    elif g.mean() / (mid_pixels.mean() * 3) < secondary_threshold:
+    elif g_mean / (mid_pixels_mean * 3) < secondary_threshold:
         return 'magenta'
-    elif r.mean() / (mid_pixels.mean() * 3) < secondary_threshold:
+    elif r_mean / (mid_pixels_mean * 3) < secondary_threshold:
         return 'cyan'
     else:
-        return 0
+        return None
 
 
 # onscreen text
@@ -228,25 +272,4 @@ def onscreen_text(frame):
         return False
 
 
-# faces
-def face_locations(film, frame_number):
-    frame_folder = os.path.join('../frame_per_second', film)
-    img_path = frame_folder + '/' + film + '_frame' + str(frame_number) + '.jpg'
-    image = face_recognition.load_image_file(img_path)
-    locations = face_recognition.face_locations(image, number_of_times_to_upsample=1)
 
-    print('Found ' + str(len(locations)) + ' face(s) in frame ' + str(frame_number))
-
-    return locations
-
-
-def primary_character(locations):
-    first_face = locations.pop(0)
-    first_face_size = first_face[2] - first_face[0]
-    primary_char_flag = 1
-    primary_char_threshold = .75  # 75% of the first face
-    for face in locations:
-        if face[2] - face[0] > (first_face_size * primary_char_threshold):
-            primary_char_flag = 0
-
-    return primary_char_flag
