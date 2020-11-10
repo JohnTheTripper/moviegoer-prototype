e94172a23cb9a10b5c547fd5ea6cd0501e36bd74
JohnTheTripper <minutehand@gmail.com>
2020-11-09 23:45:23 -0500

    generate_characters() to automatically create character dictionaries

--- unifying_features/character_identification_io.py
+++ unifying_features/character_identification_io.py
@@ -7,9 +7,11 @@ from vision_dataframes_io import *
 sys.path.append('../audio_features')
 from audio_dataframes_io import *
 from time_reference_io import *
+from collections import Counter
 
 
-def get_primary_chars(sentences, subtitle_df, nlp):
+def get_primary_chars(sentence_df, subtitle_df, nlp):
+    sentences = sentence_df['sentence'].tolist()
     chars_sub_mentions = character_subtitle_mentions(sentences, nlp)
     chars_offscreen_speakers = character_offscreen_speakers(subtitle_df)
 
@@ -82,3 +84,109 @@ def self_intro_average_encoding(movie_choice, char_name, sentence_df, subtitle_d
         average_encoding = None
 
     return average_encoding
+
+
+def generate_character_clusters(scene_dictionaries, common_count=5):
+    # search all scenes for anchor face clusters, flatten, and then generate list of most common
+    anchor_face_cluster_lists = []
+
+    x = 1
+
+    while x <= len(scene_dictionaries):
+        scene_left_list = []
+
+        scene_left_list.append(scene_dictionaries[x]['left_anchor_face_cluster'])
+
+        for matching_left_face in scene_dictionaries[x]['matching_left_face_clusters']:
+            scene_left_list.append(matching_left_face)
+
+        scene_right_list = []
+
+        scene_right_list.append(scene_dictionaries[x]['right_anchor_face_cluster'])
+
+        for matching_right_face in scene_dictionaries[x]['matching_right_face_clusters']:
+            scene_right_list.append(matching_right_face)
+
+        anchor_face_cluster_lists.append(scene_left_list)
+        anchor_face_cluster_lists.append(scene_right_list)
+
+        x += 1
+
+    flattened_anchor_list = []
+    for anchor_list in anchor_face_cluster_lists:
+        for anchor in anchor_list:
+            flattened_anchor_list.append(anchor)
+
+    anchor_character_counter = Counter(flattened_anchor_list)
+    most_common_anchors = []
+
+    for anchor in anchor_character_counter.most_common(common_count):
+        most_common_anchors.append(anchor[0])
+
+    # input most common anchors, output character cluster list
+    character_cluster_list = []
+
+    for character in most_common_anchors:
+        matching_clusters = [character]
+        search_clusters = []
+        search_continues = 1
+
+        # search face clusters of all scenes to find matching clusters, then repeat search until no more found
+        while search_continues == 1:
+            if search_clusters:
+                matching_clusters = search_clusters.copy()
+            for anchor_face_clusters in anchor_face_cluster_lists:
+                for matching_cluster in matching_clusters:
+                    if matching_cluster in anchor_face_clusters:
+                        for anchor_face in anchor_face_clusters:
+                            search_clusters.append(anchor_face)
+            search_clusters = list(set(search_clusters))
+
+            if sorted(matching_clusters) == sorted(search_clusters):
+                search_continues = 0
+                character_cluster_list.append(sorted(matching_clusters))
+
+    # remove duplicates
+    unique_character_clusters = []
+    for character_clusters in character_cluster_list:
+        if character_clusters not in unique_character_clusters:
+            unique_character_clusters.append(character_clusters)
+
+    return unique_character_clusters
+
+
+def generate_characters(scene_dictionaries):
+    character_clusters = generate_character_clusters(scene_dictionaries)
+    character_dictionary_list = []
+
+    y = 1
+    for single_character_face_clusters in character_clusters:
+
+        character_scenes = []
+
+        x = 1
+        while x <= len(scene_dictionaries):
+            for cluster in single_character_face_clusters:
+                if cluster == scene_dictionaries[x]['left_anchor_face_cluster'] or cluster in scene_dictionaries[x][
+                    'matching_left_face_clusters'] or cluster == scene_dictionaries[x][
+                    'right_anchor_face_cluster'] or cluster in scene_dictionaries[x]['matching_right_face_clusters']:
+                    character_scenes.append(x)
+            x += 1
+
+        character_scenes = sorted(list(set(character_scenes)))
+
+        character_dict = {'character_id': y,
+                          'face_clusters': single_character_face_clusters,
+                          'scenes_present': character_scenes}
+
+        character_dictionary_list.append(character_dict)
+        y += 1
+
+    character_dictionaries = {}
+
+    z = 1
+    for character_dict in character_dictionary_list:
+        character_dictionaries[z] = character_dict
+        z += 1
+
+    return character_dictionaries
