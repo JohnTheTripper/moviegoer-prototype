91e9cef00930e5a9ec3ebbfa225fd4ee82e064e4
JohnTheTripper <minutehand@gmail.com>
2021-02-13 23:11:06 -0500

    separated individual functions

--- unifying_features/scene_details_io.py
+++ unifying_features/scene_details_io.py
@@ -7,16 +7,12 @@ sys.path.append('../subtitle_features')
 from subtitle_dataframes_io import *
 
 
-def display_scene_dialogue_context(scene_dict, subtitle_df, sentence_df, nlp):
-    """
-    calculates and prints various information about scene-level dialogue
-    scene-level sentence_df and subtitle_df are created and used for calculations and phrase searching
-    doesn't return any values - this code can form the basis of other functions
-    useful for comparing with display_film_baseline(), though some *per minute* stats don't cleanly compare
-    """
-    scene_duration = scene_dict['last_frame'] + 1 - scene_dict['first_frame']
+def generate_scene_level_dfs(scene_dict, vision_df, face_df, subtitle_df, sentence_df):
+    scene_vision_df = vision_df[scene_dict['first_frame'] - 1:scene_dict['last_frame']]
+    scene_face_df = face_df[scene_dict['first_frame'] - 1:scene_dict['last_frame']]
     scene_start_time = frame_to_time(scene_dict['first_frame'])
-    scene_end_time = frame_to_time(scene_dict['last_frame'] + 1) # add 1 second; scene ends one second after this frame is onscreen
+    scene_end_time = frame_to_time(
+        scene_dict['last_frame'] + 1)  # add 1 second; scene ends one second after this frame is onscreen
     scene_subtitle_df = subtitle_df[
         (subtitle_df['end_time'] > scene_start_time) & (subtitle_df['start_time'] < scene_end_time)].copy()
 
@@ -28,15 +24,51 @@ def display_scene_dialogue_context(scene_dict, subtitle_df, sentence_df, nlp):
                 scene_sentence_indices.append(x)
         x += 1
     scene_sentence_df = sentence_df[scene_sentence_indices[0]: scene_sentence_indices[-1] + 1]
-    scene_sentences = scene_sentence_df.sentence.tolist()
 
-    # cadence (sentences per minute)
+    return scene_vision_df, scene_face_df, scene_subtitle_df, scene_sentence_df
+
+
+def get_scene_cadence(scene_vision_df, scene_subtitle_df):
+    scene_duration = len(scene_vision_df)
     cadence = len(scene_subtitle_df) / (scene_duration / 60)
 
+    if round(cadence) >= 35:
+        print('This scene has a fast cadence, with a conversation speed of', round(cadence), 'sentences per minute.')
+    elif round(cadence) < 20:
+        print('This scene has a slow cadence, with a conversation speed of', round(cadence), 'sentences per minute.')
+    else:
+        print('This scene has a medium cadence, with a conversation speed of', round(cadence), 'sentences per minute.')
+
+    return cadence
+
+
+def get_scene_ppw(scene_sentence_df):
+    space_count = 0
+    sentence_list = scene_sentence_df.sentence.tolist()
+    for sentence in sentence_list:
+        for character in sentence:
+            if character.isspace():
+                space_count += 1
+
+    word_count = space_count + len(sentence_list)
+    profanity_count = scene_sentence_df.profanity.sum()
+
+    profanity_per_word = profanity_count / word_count
+
+    if profanity_per_word == 0:
+        print('The scene contains no profanity.')
+    else:
+        print('One in', round(1 / profanity_per_word), 'words is a profanity.')
+
+    return profanity_per_word
+
+
+def display_scene_important_phrases(sentence_df, scene_sentence_df, nlp):
     # tf_idf data preparation
-    film_doc = sentence_df.sentence.tolist()
+    film_doc_df = sentence_df.copy()
+    film_doc_df.drop(range(scene_sentence_df.index.start, scene_sentence_df.index.stop), inplace=True)
+    film_doc = film_doc_df.sentence.tolist()
     scene_doc = scene_sentence_df.sentence.tolist()
-    del film_doc[scene_sentence_indices[0]: scene_sentence_indices[-1] + 1]
     scene_doc_joined = (' '.join(scene_doc))
     film_doc_joined = (' '.join(film_doc))
     film_scene_doc = [scene_doc_joined, film_doc_joined]
@@ -48,6 +80,31 @@ def display_scene_dialogue_context(scene_dict, subtitle_df, sentence_df, nlp):
     tf_idf_df = tf_idf_df.sort_values('TF-IDF', ascending=False)
     tf_idf_terms = list(tf_idf_df.head(5).index)
 
+    # nlp prep
+    scene_sentence_doc = nlp((' '.join(scene_sentence_df.sentence.tolist())))
+
+    noun_groups = []
+    for group in scene_sentence_doc.noun_chunks:
+        if group.root.pos_ != 'PRON':
+            noun_groups.append(str(group))
+    ng_count = Counter(noun_groups)
+    ng_terms = []
+    for ng in ng_count.most_common(5):
+        ng_terms.append(ng[0])
+
+    print('--------------------------------')
+    print('Possible Important Terms, TF-IDF')
+    print('--------------------------------')
+    print(tf_idf_terms)
+    print()
+    print()
+    print('-------------------------------------')
+    print('Possible Important Terms, Noun Groups')
+    print('-------------------------------------')
+    print(ng_terms)
+
+
+def display_scene_important_sentences(scene_sentence_df, nlp):
     # nlp prep
     scene_sentence_doc = nlp((' '.join(scene_sentence_df.sentence.tolist())))
     sent_nlp_list = list(scene_sentence_doc.sents)
@@ -81,43 +138,6 @@ def display_scene_dialogue_context(scene_dict, subtitle_df, sentence_df, nlp):
         x += 1
     direct_question_indices = list(set(direct_question_indices))
 
-    noun_groups = []
-    for group in scene_sentence_doc.noun_chunks:
-        if group.root.pos_ != 'PRON':
-            noun_groups.append(str(group))
-    ng_count = Counter(noun_groups)
-    ng_terms = []
-    for ng in ng_count.most_common(5):
-        ng_terms.append(ng[0])
-
-    print('-------------------------------')
-    print('Icebreaker (Conversation Start)')            # first three sentences of scene
-    print('-------------------------------')
-    print(scene_sentences[0])
-    print(scene_sentences[1])
-    print(scene_sentences[2])
-    print()
-    print()
-    print('-------------------------')
-    print('Kicker (Conversation End)')                  # final three sentences of scene
-    print('-------------------------')
-    print(scene_sentences[-3])
-    print(scene_sentences[-2])
-    print(scene_sentences[-1])
-    print()
-    print()
-    print('--------------------------------')
-    print('Possible Important Terms, TF-IDF')
-    print('--------------------------------')
-    print(tf_idf_terms)
-    print()
-    print()
-    print('-------------------------------------')
-    print('Possible Important Terms, Noun Groups')
-    print('-------------------------------------')
-    print(ng_terms)
-    print()
-    print()
     print('--------------------------------')
     print('Directed Questions and Responses')       # second-person questions directed at "you"
     print('--------------------------------')
@@ -136,34 +156,32 @@ def display_scene_dialogue_context(scene_dict, subtitle_df, sentence_df, nlp):
     print('-----------------------')
     for index in you_indices:
         print(sent_nlp_list[index])
+
+
+def display_scene_start_end(scene_sentence_df):
+    scene_sentences = scene_sentence_df.sentence.tolist()
+    print('-------------------------------')
+    print('Icebreaker (Conversation Start)')            # first three sentences of scene
+    print('-------------------------------')
+    print(scene_sentences[0])
+    print(scene_sentences[1])
+    print(scene_sentences[2])
     print()
-    print('-----------------------')
-    print('Cadence, Flow, and Vibe')                       # measurements to determine the "vibe" of the conversation
-    print('-----------------------')
-    if round(cadence) >= 35:
-        print('This scene has a fast cadence, with a conversation speed of', round(cadence), 'sentences per minute.')
-    elif round(cadence) < 20:
-        print('This scene has a slow cadence, with a conversation speed of', round(cadence), 'sentences per minute.')
-    else:
-        print('This scene has a medium cadence, with a conversation speed of', round(cadence), 'sentences per minute.')
-    print('There are', scene_subtitle_df.laugh.sum(), 'instances of laughter.')
-    print('There are', scene_subtitle_df.hesitation.sum(), 'midsentence hesitation interjections.')
-    profanity_per_word = get_profanity_per_word(scene_sentence_df)
-    if profanity_per_word == 0:
-        print('The scene contains no profanity.')
-    else:
-        print('One in', round(1 / profanity_per_word), 'words is a profanity.')
+    print()
+    print('-------------------------')
+    print('Kicker (Conversation End)')                  # final three sentences of scene
+    print('-------------------------')
+    print(scene_sentences[-3])
+    print(scene_sentences[-2])
+    print(scene_sentences[-1])
 
 
-def display_scene_emotions(scene_dict, face_df):
+def display_scene_emotions(scene_dict, scene_face_df):
     """
     prints the primary emotion for each of the two anchor characters
     primary emotion is each character's most common emotion in each frame in which they appear
     doesn't return any values - this code can form the basis of other functions
     """
-    scene_start_frame = scene_dict['first_frame']
-    scene_last_frame = scene_dict['last_frame']
-    scene_face_df = face_df.copy()[scene_start_frame - 1:scene_last_frame]
     left_face_clusters = [scene_dict['left_anchor_face_cluster']]
     for matching_cluster in scene_dict['matching_left_face_clusters']:
         left_face_clusters.append(matching_cluster)
@@ -179,13 +197,11 @@ def display_scene_emotions(scene_dict, face_df):
           ', in ' + '{:.0%}'.format(right_emotion_percentage) + ' of frames')
 
 
-def generate_scene_participants(scene_dict, subtitle_df, sentence_df):
+def generate_scene_participants(scene_subtitle_df, scene_sentence_df):
     """
     returns a best-guess at names of possible scene participants
     looks for sentences with a direct-address ('Many thanks, Adam.') or offscreen speaker label
     """
-    scene_subtitle_df, scene_sentence_df = generate_scene_sub_sent_df(scene_dict, subtitle_df, sentence_df)
-
     scene_participants = []
     for name in scene_sentence_df.direct_address.value_counts().index:
         if name[0].isupper():
