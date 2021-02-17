8d5b42cf7724040f80cb211583a5e903cf01c10c
JohnTheTripper <minutehand@gmail.com>
2021-02-17 00:54:39 -0500

    redundant function files

--- audio_features/conversations_io.py
+++ audio_features/conversations_io.py
@@ -14,9 +14,10 @@ def extract_scene_audio(film, scene_dict):
     last = str(scene_dict['last_frame'])
 
     extracted_file_name = os.path.join('../extracted_audio', film, first + '_' + last + '.wav')
-    out = ffmpeg.output(input_stream, extracted_file_name, ss=scene_dict['first_frame'], ac=2,
-                        t=scene_dict['scene_duration'])
-    ffmpeg.run(out)
+    out = ffmpeg.output(input_stream, extracted_file_name, ss=scene_dict['first_frame'], ac=2, t=scene_dict['scene_duration'])
+    ffmpeg.run(out, overwrite_output=True)
+
+    print('Extracted audio file:', extracted_file_name)
 
 
 def analyze_audible_sound(audio_file, plot=False):
